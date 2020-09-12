efe031d7e1c0ad72bbdf630ce926d589dd0ce452
Esteban David Ramirez Maradiaga <sparta117naruto@gmail.com>
2020-09-11 19:49:31 -0600

    primer commit

---
+++ RosarioWeb/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RosarioWeb
+{
+    public class Class
+    {
+        //esta clase solo se crea para realizar  commit inicial
+    }
+}
